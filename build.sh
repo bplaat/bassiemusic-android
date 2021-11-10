0d0a6e6ca5509552e5b4f84e2ec2ed7a239541d3
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2021-11-10 13:57:15 +0100

    Add macOS on ARM64 support to build script

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@
 # The default gradle Android build toolchain is so slow and produces bloated apks
 # So I use this nice build shell script to get the job done!
 
-# Install the OpenJDK JDK 8 and add all binaries to your path
+# Install an Java JDK 8 or later and add all binaries to your path
 # Install your Android SDK at ~/android-sdk/ with the following packages:
 # platform-tools, platforms;android-30, build-tools;30.0.3
-# Run this script with bash on Linux or a Git Bash / Msys install on Windows
+# Run this script with bash on Linux, macOS or a Git Bash / Msys install on Windows
 # For inspecting apks you need to install Jadx GUI and add it to your path
 
 PATH=$PATH:~/android-sdk/build-tools/30.0.3:~/android-sdk/platform-tools
@@ -40,23 +40,29 @@ else
             echo "Compiling java code"
             mkdir src-compiled
             find src -name *.java > sources.txt
-            if javac -Xlint -cp $PLATFORM -d src-compiled @sources.txt; then
+            if [ "$(uname -s)" == "Darwin" ]; then
+                # Use an Java API jar from the 1.8 version if we are using an JDK above 1.8
+                JAVAC_OPTIONS="-bootclasspath /Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Home/jre/lib/rt.jar"
+            fi
+            if javac -Xlint -source 1.8 -target 1.8 -cp $PLATFORM -d src-compiled $JAVAC_OPTIONS @sources.txt; then
 
                 echo "Packing and signing application"
                 find src-compiled -name *.class > classes.txt
-                if [ "$(uname -o)" == "Msys" ]; then
-                    d8.bat --release --lib $PLATFORM --min-api 21 @classes.txt
-                else
+                if [ "$(uname -s)" == "Linux" ]; then
                     d8 --release --lib $PLATFORM --min-api 21 @classes.txt
+                elif [ "$(uname -s)" == "Darwin" ]; then
+                    d8 --release --lib $PLATFORM --min-api 21 $(find src-compiled -name *.class)
+                else
+                    d8.bat --release --lib $PLATFORM --min-api 21 @classes.txt
                 fi
                 aapt add $name-unaligned.apk classes.dex > /dev/null
 
                 zipalign -f -p 4 $name-unaligned.apk $name.apk
 
-                if [ "$(uname -o)" == "Msys" ]; then
-                    apksigner.bat sign --v4-signing-enabled false --ks keystore.jks --ks-pass pass:$password --ks-pass pass:$password $name.apk
-                else
+                if [ "$(uname -s)" == "Linux" ] || [ "$(uname -s)" == "Darwin" ]; then
                     apksigner sign --v4-signing-enabled false --ks keystore.jks --ks-pass pass:$password --ks-pass pass:$password $name.apk
+                else
+                    apksigner.bat sign --v4-signing-enabled false --ks keystore.jks --ks-pass pass:$password --ks-pass pass:$password $name.apk
                 fi
 
                 if [ "$1" == "inspect" ]; then
