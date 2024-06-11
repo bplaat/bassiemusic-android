051eddba4162409d66d9cf5bab128a885ae38600
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-06-11 22:26:47 +0200

    Update build script and add monochrome icon

--- build.sh
+++ build.sh
@@ -5,28 +5,28 @@
 # The default gradle Android build toolchain is so slow and produces bloated apks
 # So I use this nice build shell script to get the job done!
 
-# Install the OpenJDK JDK 17 and add all binaries to your path
-# Install your Android SDK and set the $ANDROID_HOME with the path with
-# the following packages: platform-tools platforms;android-34 build-tools;34.0.0
-# Run this script with bash on Linux, macOS or a Git Bash / Msys install on Windows
-# For inspecting apks you need to install Jadx GUI and add it to your path
-
-PATH=$PATH:$ANDROID_HOME/build-tools/34.0.0:$ANDROID_HOME/platform-tools
-PLATFORM=$ANDROID_HOME/platforms/android-34/android.jar
+# Install OpenJDK JDK 17 in your system
+# Install the Android SDK and set $ANDROID_HOME with the following packages:
+#   platform-tools platforms;android-34 build-tools;34.0.0
+# Optional install Jadx GUI for inspecting apks
 
 name="bassiemusic"
 package="nl.plaatsoft.bassiemusic"
 password="bassiemusic"
 main_activity=".activities.MainActivity"
 
-if [ "$1" == "key" ]; then
+PATH=$PATH:$ANDROID_HOME/build-tools/34.0.0:$ANDROID_HOME/platform-tools
+PLATFORM=$ANDROID_HOME/platforms/android-34/android.jar
+export JAVA_VERSION=17
+
+if [ "$1" = "key" ]; then
     keytool -genkey -validity 7120 -keystore keystore.jks -keyalg RSA -keysize 4096 -storepass $password -keypass $password
 
-elif [ "$1" == "log" ]; then
+elif [ "$1" = "log" ]; then
     adb logcat -c
-    adb logcat *:E
+    adb logcat "*:E"
 
-elif [ "$1" == "clear" ]; then
+elif [ "$1" = "clear" ]; then
     echo "Clearing data and opening application"
     adb shell pm clear $package
     adb shell am start -n $package/$main_activity
@@ -35,19 +35,19 @@ else
     mkdir res-compiled
     echo "Compiling resources files"
     if aapt2 compile --dir res -o res-compiled; then
-        if aapt2 link res-compiled/*.flat --manifest AndroidManifest.xml --java src -I $PLATFORM -o $name-unaligned.apk; then
+        if aapt2 link res-compiled/*.flat --manifest AndroidManifest.xml --java src -I "$PLATFORM" -o $name-unaligned.apk; then
 
             echo "Compiling java code"
             mkdir src-compiled
-            find src -name *.java > sources.txt
-            if javac -Xlint -cp $PLATFORM -d src-compiled @sources.txt; then
+            find src -name "*.java" > sources.txt
+            if javac -Xlint -cp "$PLATFORM" -d src-compiled @sources.txt; then
 
                 echo "Packing and signing application"
-                find src-compiled -name *.class > classes.txt
+                find src-compiled -name "*.class" > classes.txt
                 if [ "$(uname -s)" == "Linux" ] || [ "$(uname -s)" == "Darwin" ]; then
-                    d8 --release --lib $PLATFORM --min-api 21 @classes.txt
+                    d8 --release --lib "$PLATFORM" --min-api 21 @classes.txt
                 else
-                    d8.bat --release --lib $PLATFORM --min-api 21 @classes.txt
+                    d8.bat --release --lib "$PLATFORM" --min-api 21 @classes.txt
                 fi
                 aapt add $name-unaligned.apk classes.dex > /dev/null
 
